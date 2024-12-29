17dec981055e96ed457d83bcd37e5aeb98ac95c9
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-12-29 22:16:12 +0100

    Displaying List of all users in the side bar except current logged in user

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ChatController extends Controller
+{
+    function index() {
+
+        $users = User::where('id', '!=', auth()->user()->id)->get();
+
+        return view('dashboard', compact('users'));
+    }
+}
